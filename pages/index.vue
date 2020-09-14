3b87e2d614308bdebc76772691d826229586acec
j-barakat <j.barakat@jordanopensource.org>
2020-09-14 16:25:27 +0300

    updated index page and removed unnessicary files

--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,33 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        er
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <Layout>
+
+    <!-- Banner -->
+    <Banner />
+
+    <!-- Services -->
+    <Services />
+
+    <!-- Get Help -->
+    <GetHelp />
+
+    <!-- Footer -->
+    <Footer />
+
+  </Layout>
 </template>
 
 <script>
-export default {}
+  import Banner from '~/components/Banner';
+  import Services from '~/components/Services';
+  import GetHelp from '~/components/GetHelp';
+  import Footer from '~/components/Footer';
+
+  export default {
+    components: {
+      Banner,
+      Services,
+      GetHelp,
+      Footer
+    }
+  }
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
