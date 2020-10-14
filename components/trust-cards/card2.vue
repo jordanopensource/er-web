0944c319da685088db209927aad84d1a73461780
j-barakat <j.barakat@jordanopensource.org>
2020-10-14 23:16:35 +0300

    created trust cards and added their content

---
+++ components/trust-cards/card2.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="trust-cards container">
+    <img class="bot" src="~/assets/images/group-9.svg" alt="bot" />
+    <h3>{{ title }}</h3>
+    <p>{{ description }}</p>
+</div>
+</template>
+
+<script>
+    export default {
+
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            arabicName: {
+                type: String,
+                required: true
+            },
+        },
+
+
+        data() {
+            return {
+                title: 'شركاءٌ معتمدون',
+                description:'منها منظمة RARENET الدولية للاستجابة الرقمية ومنظمة AccessNow',
+
+            }
+        },
+
+    }
+</script>
+
+<style scoped>
+    p {
+        @apply font-theMixArab self-center;
+    }
+</style>
---
+++ components/trust-cards/card6.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="trust-cards container">
+    <img class="bot" src="~/assets/images/group-10.svg" alt="bot" />
+    <h3>{{ title }}</h3>
+    <p>{{ description }}</p>
+</div>
+</template>
+
+<script>
+    export default {
+
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            arabicName: {
+                type: String,
+                required: true
+            },
+        },
+
+
+        data() {
+            return {
+                title: 'خدمة مجانية',
+                description:'نحن منظمة غير ربحية ولن نطلب أي مبلغ مادي لقاء الخدمة المقدمة',
+
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    p {
+        @apply font-theMixArab self-center;
+    }
+
+</style>
