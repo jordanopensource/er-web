834d157a16e8ea6d027ccdb83f501976a3f34369
wissam <wissam_ataleh@outlook.com>
2020-11-24 17:52:52 +0200

    Update typography style

--- components/TrustCard.vue
+++ components/TrustCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-start items-start bg-white">
     <img :src="image" alt="bot" />
-    <h3 class="mt-6 mb-3">{{ title }}</h3>
+    <h2 class="mt-6 mb-3">{{ title }}</h2>
     <p>{{ description }}</p>
   </div>
 </template>
